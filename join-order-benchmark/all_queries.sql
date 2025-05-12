2aa6b87d118aec8aed2ef7e6cc5faf1b6b2644e1
Vimukthi Pahasara <vimukthipahasara.gg@gmail.com>
2025-05-12 16:47:16 +0530

    Correct queries that uses keywords wrongly

--- join-order-benchmark/all_queries.sql
+++ join-order-benchmark/all_queries.sql
@@ -20,7 +20,7 @@ WHERE ci.note LIKE '%(voice)%'
   AND cn.id = mc.company_id
   AND ct.id = mc.company_type_id;
 
-SELECT MIN(chn.name) AS character,
+SELECT MIN(chn.name) AS character_name,
        MIN(t.title) AS russian_mov_with_actor_producer
 FROM char_name AS chn,
      cast_info AS ci,
@@ -41,7 +41,7 @@ WHERE ci.note LIKE '%(producer)%'
   AND cn.id = mc.company_id
   AND ct.id = mc.company_type_id;
 
-SELECT MIN(chn.name) AS character,
+SELECT MIN(chn.name) AS character_name,
        MIN(t.title) AS movie_with_american_producer
 FROM char_name AS chn,
      cast_info AS ci,
